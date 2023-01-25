59abf3ece6691666641fa5650a3e615a98122f08
FrancescFe <fraferru@gmail.com>
2023-01-25 22:09:52 +0100

    feature: mockito added & test modified

--- src/test/java/com/api/pokemon/restful/api/service/PokemonServiceImplTest.java
+++ src/test/java/com/api/pokemon/restful/api/service/PokemonServiceImplTest.java
@@ -1,36 +1,57 @@
 package com.api.pokemon.restful.api.service;
 
 import com.api.pokemon.restful.api.model.Pokemon;
+import com.api.pokemon.restful.api.repository.PokemonRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+
 
 public class PokemonServiceImplTest {
-    @Autowired
-    PokemonService pokemonService;
 
-    List<Pokemon> pokemonListTop3Heaviest = pokemonService.getPokemonHeaviest();
+    @Mock
+    private PokemonRepository pokemonRepository;
 
-    List<Pokemon> pokemonHeaviestListExpected = Arrays.
-            asList(new Pokemon(null,"snorlax",4600,null,null),
-                    new Pokemon(null,"golem",3000,null,null),
-                    new Pokemon(null,"gyarados",2350,null,null));
+    @InjectMocks
+    private PokemonServiceImpl pokemonServiceImpl;
 
-    @Test
-    void pokemonHeaviestTop3NamesOk(){
-        assertEquals(pokemonHeaviestListExpected.stream().map(Pokemon::getName),
-                pokemonListTop3Heaviest.stream().map(Pokemon::getName));
+    private List<Pokemon> pokemonList;
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        pokemonList = Arrays.
+                asList(new Pokemon(null,"snorlax",4600,null,null),
+                        new Pokemon(null,"golem",3000,null,null),
+                        new Pokemon(null,"gyarados",2350,null,null));
+
     }
 
     @Test
-    void pokemonHeaviestTop3WeightOrderOk(){
-        assertEquals(pokemonHeaviestListExpected.stream().map(Pokemon::getWeight).sorted(Comparator.reverseOrder()),
-                pokemonListTop3Heaviest.stream().map(Pokemon::getWeight));
+    void getPokemonHeaviest() {
+        when(pokemonServiceImpl.getPokemonHeaviest()).thenReturn(pokemonList);
+
+        assertEquals("snorlax", pokemonList.get(0).getName());
+        assertEquals("golem", pokemonList.get(1).getName());
+        assertEquals("gyarados", pokemonList.get(2).getName());
+        assertEquals(4600, pokemonList.get(0).getWeight());
+        assertEquals(3000, pokemonList.get(1).getWeight());
+        assertEquals(2350, pokemonList.get(2).getWeight());
+
+        //verify(pokemonServiceImpl).getPokemonHeaviest();
+    }
+
+    @Test
+    void updatePokemon() {
     }
 
 }
